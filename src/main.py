978eab71cb09a1a3c9372ccec491d9a9be66aca3
rafaeldcx <rafaeldcx@gmail.com>
2024-09-20 20:39:32 -0300

    Notebooks

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ recursos = Rec.Recursos.get_instance()
 
 def main():
    # print(f"ID da instância de Recursos: {id(recursos)}")
-    recursos.criar_recursos_pydriller("../algorithm-ownership/data/data_mbed-os.csv")
+    recursos.criar_recursos_pydriller("../algorithm-ownership/data/data_zephyr.csv")
     
     mom = Mom.Mom()
     mom.calcular_propriedade_mom()
-    mom.save("result_mbed-os.csv")
+    mom.save("result_data_zephyr.csv")
 
 if __name__ == "__main__":
     main()
